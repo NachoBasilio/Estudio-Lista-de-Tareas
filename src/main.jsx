7c73c3f06abfe224107d2b1f6401667fc2561070
IgnaDev <arcortoon@gmail.com>
2022-11-16 16:30:48 -0300

    Iniciando el proyecto

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import TodoApp from './components/TodoApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <TodoApp />
   </React.StrictMode>
 )

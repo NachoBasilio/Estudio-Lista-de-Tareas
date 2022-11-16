94f7741daf5999dd5f468f2b638f7965ca551ae9
IgnaDev <arcortoon@gmail.com>
2022-11-16 17:27:27 -0300

    Componente de formulario terminado

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -2,22 +2,33 @@ import React, { useState } from 'react'
 
 export default function TodoApp() {
   const [todoTitle, setTodoTitle] = useState("")
+  const [todos, setTodos] = useState([])
 
   function handleChange(e){
     setTodoTitle(e.target.value)
   }
-  function handleClick(e){
+
+  function handleSubmit(e){
     e.preventDefault()
+    
+    const newTodo = {
+      id: crypto.randomUUID(),
+      title: todoTitle,
+    }
+    setTodos([...todos, newTodo])
+    console.log(todos)
   }
 
   return (
     <>
     <div className='todoContainer'>
-      <form className='todoCreateForm'>
+      <form className='todoCreateForm' onSubmit={handleSubmit}>
         <input className='todoInput' type="text" value={todoTitle} onChange={handleChange}/>
-        <input className='buttonCreate' type="submit" value="Crear" onClick={handleClick} />
-        {todoTitle}
+        <input className='buttonCreate' type="submit" value="Crear" />
       </form>
+      <div className='todosContainer'>
+        {todos.map(todo=>(<div key={todo.id}>{todo.title}</div>))}
+      </div>
     </div>
     </>
   )

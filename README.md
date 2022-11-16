f6a80052874bb12481ee1e95778ded5fe0660788
IgnaDev <arcortoon@gmail.com>
2022-11-16 16:44:39 -0300

    Comenzamos a aplicar estados

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Lista-de-Tareas
 Primer proyecto para practicar react, este es de una dificultad baja.
+Con este tipo de proyecto podemos integrar lo básico de react, teniendo en cuenta que recién estoy iniciando tiene mucho sentido.
--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function TodoApp() {
+  const [todoTitle, setTodoTitle] = useState("")
+
+  function handleChange(e){
+    setTodoTitle(e.target.value)
+  }
+  function handleClick(e){
+    e.preventDefault()
+  }
+
   return (
-    <div>TodoApp</div>
+    <>
+    <div className='todoContainer'>
+      <form className='todoCreateForm'>
+        <input className='todoInput' type="text" value={todoTitle} onChange={handleChange}/>
+        <input className='buttonCreate' type="submit" value="Crear" onClick={handleClick} />
+        {todoTitle}
+      </form>
+    </div>
+    </>
   )
 }
